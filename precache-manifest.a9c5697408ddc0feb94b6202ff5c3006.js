4e4d2a767ccae4cc046b32ea08bbe8bbd189c8a4
sershishkov <ser.shishkov@gmail.com>
2018-10-31 18:13:07 +0200

    Updates

---
+++ precache-manifest.a9c5697408ddc0feb94b6202ff5c3006.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c84aaaec5e2976f9cb",
+    "url": "/sh_portfolio/static/js/runtime~main.35c84aaa.js"
+  },
+  {
+    "revision": "70b5b1943daacb8721e5",
+    "url": "/sh_portfolio/static/js/main.70b5b194.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/sh_portfolio/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "70d5c9245436cd82e393577e1c6deec8",
+    "url": "/sh_portfolio/index.html"
+  }
+];
